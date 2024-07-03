39c71add41f19ef7825d88bae08c847ad82d43e5
Ritesh-Dev1 <ritesh170202@gmail.com>
2024-07-03 23:34:43 +0530

    add mobile menu and new project card

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //add javascript
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
